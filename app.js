49f9c57784a0e6bffab6616b786b6d41566c5add
anirudhsingh05 <anirudhsingh05032001@gmail.com>
2024-03-02 10:43:26 +0530

    first commit- learned to create components

--- app.js
+++ app.js
@@ -43,18 +43,193 @@ const Headers = () => {
   );
 };
 
-const RestaurantCard = () => {
+const imgURL =
+  "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/";
+
+const resobj = [
+  {
+    info: {
+      id: "607353",
+      name: "The Filter Coffee",
+      cloudinaryImageId: "hcgo1vj7mikezkmqcroh",
+      locality: "Bannergatta Main Road",
+      areaName: "Arekere",
+      costForTwo: "₹250 for two",
+      cuisines: ["South Indian", "Snacks", "Beverages"],
+      avgRating: 4.7,
+      veg: true,
+      parentId: "21001",
+      avgRatingString: "4.7",
+      totalRatingsString: "5K+",
+      sla: {
+        deliveryTime: 36,
+        lastMileTravel: 4.9,
+        serviceability: "SERVICEABLE",
+        slaString: "35-40 mins",
+        lastMileTravelString: "4.9 km",
+        iconType: "ICON_TYPE_EMPTY",
+      },
+      availability: {
+        nextCloseTime: "2024-03-02 22:00:00",
+        opened: true,
+      },
+      badges: {
+        imageBadges: [
+          {
+            imageId: "v1695133679/badges/Pure_Veg111.png",
+            description: "pureveg",
+          },
+        ],
+      },
+      isOpen: true,
+      aggregatedDiscountInfoV2: {},
+      type: "F",
+      badgesV2: {
+        entityBadges: {
+          imageBased: {
+            badgeObject: [
+              {
+                attributes: {
+                  description: "pureveg",
+                  imageId: "v1695133679/badges/Pure_Veg111.png",
+                },
+              },
+            ],
+          },
+          textBased: {},
+          textExtendedBadges: {},
+        },
+      },
+      orderabilityCommunication: {
+        title: {},
+        subTitle: {},
+        message: {},
+        customIcon: {},
+      },
+      differentiatedUi: {
+        displayType: "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
+        differentiatedUiMediaDetails: {
+          mediaType: "ADS_MEDIA_ENUM_IMAGE",
+          lottie: {},
+          video: {},
+        },
+      },
+      reviewsSummary: {},
+      displayType: "RESTAURANT_DISPLAY_TYPE_DEFAULT",
+      restaurantOfferPresentationInfo: {},
+    },
+    analytics: {
+      context: "seo-data-27614963-a8e0-4167-859c-2276d963e411",
+    },
+    cta: {
+      link: "https://www.swiggy.com/restaurants/the-filter-coffee-bannergatta-main-road-arekere-bangalore-607353",
+      text: "RESTAURANT_MENU",
+      type: "WEBLINK",
+    },
+    widgetId: "collectionV5RestaurantListWidget_SimRestoRelevance_food_seo",
+  },
+  {
+    info: {
+      id: "376345",
+      name: " Krishnam Udupi Hotel",
+      cloudinaryImageId: "hhdmpjfcu0r3hlsmsnly",
+      locality: "Konanakunte",
+      areaName: "Kanakapura Road",
+      costForTwo: "₹500 for two",
+      cuisines: [
+        "South Indian",
+        "North Indian",
+        "Ice Cream",
+        "Chaat",
+        "Beverages",
+      ],
+      avgRating: 4.5,
+      veg: true,
+      parentId: "121050",
+      avgRatingString: "4.5",
+      totalRatingsString: "10K+",
+      sla: {
+        deliveryTime: 24,
+        lastMileTravel: 2.4,
+        serviceability: "SERVICEABLE",
+        slaString: "20-25 mins",
+        lastMileTravelString: "2.4 km",
+        iconType: "ICON_TYPE_EMPTY",
+      },
+      availability: {
+        nextCloseTime: "2024-03-02 22:15:00",
+        opened: true,
+      },
+      badges: {},
+      isOpen: true,
+      type: "F",
+      badgesV2: {
+        entityBadges: {
+          imageBased: {},
+          textBased: {},
+          textExtendedBadges: {},
+        },
+      },
+      aggregatedDiscountInfoV3: {
+        header: "₹100 OFF",
+        subHeader: "ABOVE ₹499",
+        discountTag: "FLAT DEAL",
+      },
+      orderabilityCommunication: {
+        title: {},
+        subTitle: {},
+        message: {},
+        customIcon: {},
+      },
+      differentiatedUi: {
+        displayType: "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
+        differentiatedUiMediaDetails: {
+          mediaType: "ADS_MEDIA_ENUM_IMAGE",
+          lottie: {},
+          video: {},
+        },
+      },
+      reviewsSummary: {},
+      displayType: "RESTAURANT_DISPLAY_TYPE_DEFAULT",
+      restaurantOfferPresentationInfo: {},
+    },
+    analytics: {
+      context: "seo-data-27614963-a8e0-4167-859c-2276d963e411",
+    },
+    cta: {
+      link: "https://www.swiggy.com/restaurants/-krishnam-udupi-hotel-konanakunte-kanakapura-road-bangalore-376345",
+      text: "RESTAURANT_MENU",
+      type: "WEBLINK",
+    },
+    widgetId: "collectionV5RestaurantListWidget_SimRestoRelevance_food_seo",
+  },
+];
+
+const RestaurantCard = (props) => {
+  const { resData } = props;
+  const {
+    cloudinaryImageId,
+    name,
+    cuisines,
+    sla,
+    avgRatingString,
+    costForTwo,
+  } = resData.info;
   return (
     <div className="res-card">
       <img
         className="res-logo"
         alt="res-logo"
-        src="https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/wet9zrjusmdtkk16jlrz"
+        src={imgURL + cloudinaryImageId}
       />
-      <h3>Meghana Foods</h3>
-      <h4>North Indian, Asians</h4>
-      <h4>38 minutes</h4>
-      <h4>4.4 stars</h4>
+      <h3>{name}</h3>
+      <h4>{cuisines.join(", ")}</h4>
+      <h4>
+        {sla.slaString} {}
+      </h4>
+      <h4>
+        {avgRatingString} stars {costForTwo}
+      </h4>
     </div>
   );
 };
@@ -64,9 +239,11 @@ const Body = () => {
     <div className="body">
       <div className="search">search</div>
       <div className="res-container">
-        <RestaurantCard /> <RestaurantCard /> <RestaurantCard />
-        <RestaurantCard /> <RestaurantCard /> <RestaurantCard />
-        <RestaurantCard /> <RestaurantCard /> <RestaurantCard />
+        {/* <RestaurantCard resData={resobj[0]} />
+        <RestaurantCard resData={resobj[1]} /> */}
+        {resobj.map((Restaurant) => (
+          <RestaurantCard key={Restaurant.info.id} resData={Restaurant} />
+        ))}
       </div>
     </div>
   );
