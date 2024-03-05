2df7c16e7961659010b6b2ad3425972f7aafea60
anirudhsingh05 <anirudhsingh05032001@gmail.com>
2024-03-05 10:55:12 +0530

    Learned about react hooks and Reconsiliation

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,14 +1,29 @@
 import resobjlist from "../utils/mockData";
 import RestaurantCard from "./RestaurantCard";
+import { useState } from "react";
 
 const Body = () => {
+  const [listOfRestaurants, setListOfRestaurants] = useState(resobjlist);
+
   return (
     <div className="body">
-      <div className="search">search</div>
+      <div className="filter">
+        <button
+          className="filter-btn"
+          onClick={() => {
+            const filteredList = listOfRestaurants.filter(
+              (res) => res.info.avgRating > 4.4
+            );
+            setListOfRestaurants(filteredList);
+          }}
+        >
+          Top rated Restaurants
+        </button>
+      </div>
       <div className="res-container">
         {/* <RestaurantCard resData={resobjlist[0]} />
           <RestaurantCard resData={resobjlist[1]} /> */}
-        {resobjlist.map((Restaurant) => (
+        {listOfRestaurants.map((Restaurant) => (
           <RestaurantCard key={Restaurant.info.id} resData={Restaurant} />
         ))}
       </div>
