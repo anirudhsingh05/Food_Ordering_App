8424afb40a809be7117e935e3a4f861b7da9f83c
anirudhsingh05 <anirudhsingh05032001@gmail.com>
2024-03-08 13:26:27 +0530

    Used Swiggy api to fetch data and rendered it

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,9 +1,23 @@
-import resobjlist from "../utils/mockData";
 import RestaurantCard from "./RestaurantCard";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 const Body = () => {
-  const [listOfRestaurants, setListOfRestaurants] = useState(resobjlist);
+  const [listOfRestaurants, setListOfRestaurants] = useState([]);
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const fetchData = async () => {
+    const data = await fetch(
+      "https://www.swiggy.com/dapi/restaurants/list/v5?lat=12.9351929&lng=77.62448069999999&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
+    );
+    const responsedData = await data.json();
+    setListOfRestaurants(
+      responsedData?.data?.cards[4]?.card?.card?.gridElements?.infoWithStyle
+        ?.restaurants
+    );
+  };
 
   return (
     <div className="body">
