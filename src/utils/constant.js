9788bc5eb077f6224debbb7b2b4434c2a5d05e04
Anirudh Singh <64956321+anirudhsingh05@users.noreply.github.com>
2024-04-20 07:19:56 +0530

    Update constant.js
    Fixed Cors Error

--- src/utils/constant.js
+++ src/utils/constant.js
@@ -5,4 +5,4 @@ export const LOGO_URL =
   "https://static.vecteezy.com/system/resources/thumbnails/008/687/818/small/food-delivery-logo-free-vector.jpg";
 
 export const MENU_URL =
-  "https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=12.9351929&lng=77.62448069999999&restaurantId=";
+  "https://thingproxy.freeboard.io/fetch/https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=12.9351929&lng=77.62448069999999&restaurantId=";
